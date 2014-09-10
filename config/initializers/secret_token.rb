e2bf30a1c2e160615f162f12a2be7b0f90d90ce3
olumuyiwa <somide1@umbc.edu>
2014-09-10 06:40:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Somide1DemoApp::Application.config.secret_key_base = '502e41dbfba4899a2c11a31897c5b770938388268eacd5489a607ee946fdd0d6ffc684afea80859039ca1874121f122146d1837a0fc8ab5f9bf95fd9620e8d1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Somide1DemoApp::Application.config.session_store :cookie_store, key: '_somide1_demo_app_session'
