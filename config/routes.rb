b095282dbd969832e2ba581938355b3d894552ba
olumuyiwa <somide1@umbc.edu>
2014-09-10 08:32:59 -0400

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Somide1DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
